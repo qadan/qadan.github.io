4c21fe775bbc92c593a7c525414d622ac85ca752
qadan <daitken@discoverygarden.ca>
2020-01-30 22:34:00 -0400

    also a thing

--- js/prepopulate_yaml.js
+++ js/prepopulate_yaml.js
@@ -5,9 +5,65 @@
 
 /**
  * Attempts to parse an existing YAML document.
+ *
+ * @param File file
+ *   A file object.
  */
-function parse_existing_yaml(file) {
-  console.log(files);
+async function parse_existing_yaml(file) {
+  var yaml = await file.text();
+  try {
+    return jsyaml.safeLoad(yaml);
+  }
+  catch (e) {
+    pop_bad_weights_modal('Something went wrong loading this YAML file; try fixing it or use a different one?');
+  }
+}
+
+/**
+ * Helper to get the integer form of a value.
+ *
+ * @param mixed value
+ *   Something to try to turn into an integer.
+ *
+ * @return int
+ *   The number, if it was parsed, or 0.
+ */
+function get_int(value) {
+  var as_int = parseInt(value);
+  if (typeof as_int == 'number') {
+    return as_int;
+  }
+  return 0;
+}
+
+/**
+ * Runs through a list of weights, using them to prepopulate the form.
+ *
+ * @param object weights
+ *   Parsed YAML weights object.
+ */
+function prepopulate_form(weights) {
+  for (section in weights) {
+    if (section != 'rom') {
+      for (option in weights[section]) {
+        var as_int = get_int(weights[section][option]);
+        if (document.getElementById(section + '--' + option) != null) {
+          document.getElementById(section + '--' + option).value = as_int;
+          document.getElementById(section + '--' + option + '--value').innerHTML = as_int;
+        }
+      }
+    }
+  }
+  for (section in weights['rom']) {
+    for (option in weights['rom'][section]) {
+      var as_int = get_int(weights['rom'][section][option]);
+      document.getElementById(section + '--' + option).value = as_int;
+      document.getElementById(section + '--' + option + '--value').innerHTML = as_int;
+    }
+  }
+  if (weights.hasOwnProperty('description')) {
+    document.getElementById('yaml-description').value = weights['description'];
+  }
 }
 
 /**
@@ -18,20 +74,28 @@ function add_drop_event_listener() {
 
   drop_zone.ondrop = function(e) {
     e.preventDefault();
-    this.className = 'upload-drop-zone';
-    var yaml = parse_existing_yaml(e.dataTransfer.files[0]);
-    if (yaml) {
-      prepopulate_form(yaml);
-    }
+    this.className = 'card mx-auto text-white bg-dark';
+    document.getElementById('yaml-filename').value = e.dataTransfer.files[0].name;
+    parse_existing_yaml(e.dataTransfer.files[0]).then(function(yaml) {
+      if (yaml) {
+        prepopulate_form(yaml);
+        document.getElementById('drag-and-drop-text').innerHTML = "You can drag another YAML file here to re-populate the form.";
+      }
+      else {
+        document.getElementById('drag-and-drop-text').innerHTML = "Drag and drop an existing weights YAML here to pre-populate the form.";
+      }
+    });
   };
 
   drop_zone.ondragover = function() {
-    this.className = 'upload-drop-zone drop';
+    this.className = 'card mx-auto bg-light';
+    document.getElementById('drag-and-drop-text').innerHTML = "Drop weights YAML here."
     return false;
   };
 
   drop_zone.ondragleave = function() {
-    this.className = 'upload-drop-zone';
+    this.className = 'card mx-auto text-white bg-dark';
+    document.getElementById('drag-and-drop-text').innerHTML = "Drag and drop an existing weights YAML here to pre-populate the form.";
     return false;
   };
 }
